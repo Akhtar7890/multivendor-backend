2087f1b6176afcbf71bef80256f9ea7ee9e8029c
Akhtar <akhtarbabu7459941478@gmail.com>
2026-03-06 15:07:28 +0530

    fixed cors for versel

--- src/main/java/com/example/Multi_Vendor_Store/config/SecurityConfig.java
+++ src/main/java/com/example/Multi_Vendor_Store/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000")); // Explicit origins
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","https://multivendor-frontend-two.vercel.app")); // Explicit origins
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
